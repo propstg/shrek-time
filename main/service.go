93f63eabbf1d5eadbf722d0cb133c512f78ff9f4
gregory propst <g@prop.st>
2018-09-02 19:55:35 -0400

    tabs to spaces

--- main/service.go
+++ main/service.go
@@ -1,24 +1,24 @@
 package shrek
 
 import (
-	"time"
+    "time"
 )
 
 const ONE_SHREK float64 = 93.0 * 60.0
 
 func Now() (float64) {
-	currentTime := time.Now().UTC()
-	return ToShrek(currentTime)
+    currentTime := time.Now().UTC()
+    return ToShrek(currentTime)
 }
 
 func ToShrek(timeToConvert time.Time) (float64) {
-	shrepoch := time.Date(2001, time.April, 22, 7, 0, 0, 0, time.UTC)
-	secondsSinceShrepoch := timeToConvert.Sub(shrepoch).Seconds()
-	return secondsSinceShrepoch / ONE_SHREK
+    shrepoch := time.Date(2001, time.April, 22, 7, 0, 0, 0, time.UTC)
+    secondsSinceShrepoch := timeToConvert.Sub(shrepoch).Seconds()
+    return secondsSinceShrepoch / ONE_SHREK
 }
 
 func FromShrek(shrekTime float64) (time.Time) {
-	shrepoch := time.Date(2001, time.April, 22, 7, 0, 0, 0, time.UTC)
-	secondsSinceShrepoch := time.Duration(ONE_SHREK * shrekTime) * time.Second
-	return shrepoch.Add(secondsSinceShrepoch)
+    shrepoch := time.Date(2001, time.April, 22, 7, 0, 0, 0, time.UTC)
+    secondsSinceShrepoch := time.Duration(ONE_SHREK * shrekTime) * time.Second
+    return shrepoch.Add(secondsSinceShrepoch)
 }
--- main/service_test.go
+++ main/service_test.go
@@ -1,17 +1,17 @@
 package shrek
 
 import (
-	"testing"
-	"github.com/stretchr/testify/assert"
-	"time"
+    "testing"
+    "github.com/stretchr/testify/assert"
+    "time"
 )
 
 func TestToShrek(t *testing.T) {
-	assert.Equal(t, float64(100000), ToShrek(time.Date(2018, time.December, 27, 15, 0, 0, 0, time.UTC)))
-	assert.Equal(t, float64(0), ToShrek(time.Date(2001, time.April, 22, 7, 0, 0, 0, time.UTC)))
+    assert.Equal(t, float64(100000), ToShrek(time.Date(2018, time.December, 27, 15, 0, 0, 0, time.UTC)))
+    assert.Equal(t, float64(0), ToShrek(time.Date(2001, time.April, 22, 7, 0, 0, 0, time.UTC)))
 }
 
 func TestFromShrek(t *testing.T) {
-	assert.Equal(t, FromShrek(float64(100000)), time.Date(2018, time.December, 27, 15, 0, 0, 0, time.UTC))
-	//assert.Equal(t, FromShrek(float64(0)), time.Date(2001, time.April, 22, 7, 0, 0, 0, time.UTC))
+    assert.Equal(t, FromShrek(float64(100000)), time.Date(2018, time.December, 27, 15, 0, 0, 0, time.UTC))
+    assert.Equal(t, FromShrek(float64(0)), time.Date(2001, time.April, 22, 7, 0, 0, 0, time.UTC))
 }
